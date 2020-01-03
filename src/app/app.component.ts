e4a1f881a7993f4ea5b6608b540755e59453095d
chaty <kbabachaitanya@gmail.com>
2020-01-03 17:54:01 -0500

    playing around with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,27 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DownstreamService } from './downstream.service';
-import { CreditModel } from './credit-model';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { TotalByLocation } from './total-by-location';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
-  creditTotal: number = 0;
-  debitTotal: number = 0;
   totalByLocation: TotalByLocation[];
   totalAmount: number;
-  selectedTotal: number;
 
-  displayedColumns: string[] = ['date', 'location', 'debit', 'credit'];
-  dataSource: MatTableDataSource<CreditModel>;
-
-  expandDataSource: MatTableDataSource<TotalByLocation>;
-  expandTableColumns: string[] = ['location', 'creditTotal', 'debitTotal'];
-  expandedElement: TotalByLocation | null;
-
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private app: DownstreamService) { }
 
   ngOnInit() {
 
-    this.app.getCreditData().subscribe(
-      resp => {
-        this.dataSource = new MatTableDataSource(resp.body);
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      }
-    );
 
     this.app.getCreditMap().subscribe(
       resp => {
         this.totalByLocation = resp.body;
-        this.expandDataSource = new MatTableDataSource(resp.body);
         this.totalAmount = this.totalByLocation.map(row => row.creditTotal).reduce((a, b) => a + b, 0);
       }
     );
@@ -61,23 +29,6 @@ export class AppComponent implements OnInit {
 
   }
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-    this.getTotalCost();
 
-  }
-
-  getTotalCost() {
-    var filteredData = this.dataSource.filteredData;
-    this.creditTotal = filteredData.map(row => +row.credit).reduce((a, b) => a + b, 0);
-    this.debitTotal = filteredData.map(row => +row.debit).reduce((a, b) => a + b, 0);
-  }
-
-  addTotal(list) {
-    this.selectedTotal = list.selectedOptions.selected.map(item => item.value.creditTotal).reduce((a, b) => a + b, 0);
-  }
 
 }
